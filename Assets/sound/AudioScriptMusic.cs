ff9be791d6d44a3fbd5037f27f79f8b68643fddd
blobbeliblob <blobbeliblob@gmail.com>
2019-05-11 16:13:11 +0300

    readded sound

---
+++ Assets/sound/AudioScriptMusic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class AudioScriptMusic : MonoBehaviour
+{
+
+    public AudioClip musicAudioClip;
+    public AudioSource musicAudioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicAudioSource = GetComponent<AudioSource>();
+        musicAudioSource.clip = musicAudioClip;
+
+        musicAudioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
